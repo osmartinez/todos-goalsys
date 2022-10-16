3adb3698232b33a0e30000e0a9646c9fefa2195c
Oscar <oscarmtzz22@gmail.com>
2022-10-16 19:15:54 +0200

    green hides editable input by default if no editmode enabled

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,15 +2,18 @@
     <div v-if="todo" class="todo">
         <input class="toggle" type="checkbox" v-model="todo.completed">
         <label class="todo-name">{{todo.name}}</label>
+        <input v-show="editMode" class="edit" type="text">
         <button class="btn-delete">x</button>
     </div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
 const props = defineProps({
     todo: Object,
 })
+const editMode = ref(false)
+
 
 </script>
 
--- src/components/__tests__/Todo.cy.js
+++ src/components/__tests__/Todo.cy.js
@@ -1,7 +1,7 @@
 import Todo from '../Todo.vue'
 
-const completedTodo = { id: 1, name: '#todo 1', completed: true} 
-const notCompletedTodo = { id: 1, name: '#todo 1', completed: false} 
+const completedTodo = { id: 1, name: '#todo 1', completed: true }
+const notCompletedTodo = { id: 1, name: '#todo 1', completed: false }
 
 describe('Todo', () => {
     it('renders properly', () => {
@@ -9,39 +9,46 @@ describe('Todo', () => {
     })
 
     it('shows todo name', () => {
-        cy.mount(Todo,{ props:  {todo: completedTodo}})
+        cy.mount(Todo, { props: { todo: completedTodo } })
         cy.get('.todo-name')
-        .contains('#todo 1')
-      })
+            .contains('#todo 1')
+    })
 
-      it('has checkbox unchecked because todo is not completed', ()=>{
-        cy.mount(Todo,{ props: { todo: notCompletedTodo } })
+    it('has checkbox unchecked because todo is not completed', () => {
+        cy.mount(Todo, { props: { todo: notCompletedTodo } })
         cy.get('.toggle')
-        .should('be.not.checked')
-      })
+            .should('be.not.checked')
+    })
 
-      it('has checkbox checked because todo is completed', ()=>{
-        cy.mount(Todo,{ props: { todo: completedTodo } })
+    it('has checkbox checked because todo is completed', () => {
+        cy.mount(Todo, { props: { todo: completedTodo } })
         cy.get('.toggle')
-        .should('be.checked')
-      })
+            .should('be.checked')
+    })
 
-      it('has button hidden by default', ()=>{
-        cy.mount(Todo,{ props: { todo: completedTodo } })
+    it('has button hidden by default', () => {
+        cy.mount(Todo, { props: { todo: completedTodo } })
         cy.get('.btn-delete')
-        .should('be.not.visible')
-      })
-    
-      /**
-   * This cannot be tested with current cypress api because of css:hover cannot be triggered via js
-   */
-//   it('has button visible if mouse is over', ()=>{
-//     cy.mount(Task,{ props: { item: completedTask } })
-//     cy.get('.title')
-//     .trigger('mouseover')
+            .should('be.not.visible')
+    })
 
-//     cy.get('.btn-delete')
-//     .should('be.visible')
-//   })
+    /**
+ * This cannot be tested with current cypress api because of css:hover cannot be triggered via js
+ */
+    //   it('has button visible if mouse is over', ()=>{
+    //     cy.mount(Task,{ props: { item: completedTask } })
+    //     cy.get('.title')
+    //     .trigger('mouseover')
 
+    //     cy.get('.btn-delete')
+    //     .should('be.visible')
+    //   })
+
+
+
+    it('hides editable input by default', () => {
+        cy.mount(Todo, { props: { todo: completedTodo } })
+        cy.get('.edit')
+            .should('be.not.visible')
+    })
 })
