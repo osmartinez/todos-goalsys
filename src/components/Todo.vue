bad69b882ed58fdd4bdf429c1a866682301dd1b0
Oscar <oscarmtzz22@gmail.com>
2022-10-16 19:08:20 +0200

    green shows todo name

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,11 @@
 <template>
-    
+    <div v-if="todo" class="todo">
+        <label class="todo-name">{{todo.name}}</label>
+    </div>
 </template>
 
 <script setup>
+const props = defineProps({
+    todo: Object,
+})
 </script>
--- src/components/__tests__/Todo.cy.js
+++ src/components/__tests__/Todo.cy.js
@@ -1,9 +1,16 @@
 import Todo from '../Todo.vue'
 
+const completedTodo = { id: 1, name: '#todo 1', completed: true} 
 
 describe('Todo', () => {
     it('renders properly', () => {
         cy.mount(Todo)
     })
 
+    it('shows todo name', () => {
+        cy.mount(Todo,{ props:  {todo: completedTodo}})
+        cy.get('.todo-name')
+        .contains('#todo 1')
+      })
+    
 })
