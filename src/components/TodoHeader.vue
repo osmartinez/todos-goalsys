0d468495004f257fce139fed6ab673a9ae768f66
Oscar <oscarmtzz22@gmail.com>
2022-10-16 18:48:33 +0200

    green header triggers and clears user input

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>header</h1>
+    <div id="list-header">
+        <input class="user-input" type="text" v-model="userInput" @keyup.enter="action"
+            placeholder="Type and press enter" autofocus>
+    </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['click'])
+const checkAll = ref(false)
+let userInput = ref("")
+function action() {
+    if (!userInput.value) return
+    emit('click', userInput.value)
+    userInput.value = ''
+}
 
 </script>
 
--- src/components/__tests__/TodoHeader.cy.js
+++ src/components/__tests__/TodoHeader.cy.js
@@ -4,4 +4,13 @@ describe('TodoHeader', () => {
     it('renders properly', () => {
         cy.mount(TodoHeader)
     })
+
+    it('allows user typing and clears text when user presses {enter}',()=>{
+        cy.mount(TodoHeader)
+        cy.get('.user-input')
+          .type('#task 1')
+          .should('have.value', '#task 1')
+          .type('{enter}')
+          .should('have.value','')
+      })
 })
