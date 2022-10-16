d186d67d0f4d63433055c68f4cc38a8234afe7b8
Oscar <oscarmtzz22@gmail.com>
2022-10-16 19:02:12 +0200

    add main view built from components

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-  <header>
-    <h1>todos</h1>
-  </header>
-
-  <RouterView />
+   <div>
+    <header>
+      <h1>todos</h1>
+    </header>
+    <TodoList></TodoList>
+  </div>
 </template>
+
+<script setup>
+import TodoList from './components/TodoList.vue';
+
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="todo-list">
+        <TodoHeader @click="add"></TodoHeader>
+        <RouterView />
+        <TodoFooter></TodoFooter>
+    </div>
+</template>
+
+<script setup>
+import TodoHeader from './TodoHeader.vue'
+import TodoFooter from './TodoFooter.vue';
+const list = useListStore()
+function add(userInput) {
+    
+}
+</script>
