58d37081fa2c95633a5b2fa902d9d1499b56c68b
Oscar <oscarmtzz22@gmail.com>
2022-10-16 20:18:46 +0200

    feat editing and deleting task from its own component

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,28 +1,46 @@
 <template>
     <div v-if="todo" class="todo" @dblclick="openEditMode">
-        <input v-show="!editMode" class="toggle" type="checkbox" v-model="todo.completed">
+        <input v-show="!editMode" class="toggle" type="checkbox" v-model="todo.completed" @change="updateModel">
         <label v-show="!editMode" class="todo-name">{{todo.name}}</label>
-        <input v-show="editMode" class="edit" type="text" @keyup.enter="closeEditModeAndSave" @blur="closeEditModeAndSave" @keyup.esc="closeEditModeAndDiscard">
-        <button v-show="!editMode" class="btn-delete">x</button>
+        <input v-show="editMode" class="edit" type="text" v-model="newItemName" @keyup.enter="closeEditModeAndSave" @blur="closeEditModeAndSave" @keyup.esc="closeEditModeAndDiscard">
+        <button v-show="!editMode" @click="list.delete(item)" class="btn-delete">x</button>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useTodosStore } from '../stores/todos'
+
 const props = defineProps({
     todo: Object,
 })
+const store = useTodosStore()
 const editMode = ref(false)
+const newItemName = ref("")
+
+async function updateModel(){
+    await store.update(props.todo)
+}
 
 async function closeEditModeAndSave(){    
+    if(!editMode.value) return
+    if(newItemName.value === ""){
+        store.delete(props.todo)
+    }
+    else{
+        props.todo.name = newItemName.value
+        await updateModel()
+    }
     editMode.value = false
 }
 
 function closeEditModeAndDiscard(){
+    if(!editMode.value) return
     editMode.value = false
 }
 
 function openEditMode(){
+    newItemName.value = props.todo.name
     editMode.value = true
 }
 </script>
