e8eee7a4eada3b6b5ce46c929296dba9e530e0a4
Oscar <oscarmtzz22@gmail.com>
2022-10-16 20:13:09 +0200

    green disables/enables clear completed button when there are/no completed todos

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -6,10 +6,12 @@
         <RouterLink class="filter" v-for="route in routes" v-bind:key="route.name" :to="route">{{route.text}}</RouterLink>
       </div>
   
-      <button>Clear completed</button>
+      <button :disabled="store.countCompletedTodos === 0">Clear completed</button>
     </div>
   </template>
   
   <script setup>
+  import {useTodosStore} from '../stores/todos'
   const routes = [{ name: 'allTodos', text: 'all' }, { name: 'activeTodos', text: 'active' }, { name: 'completedTodos', text: 'completed' }]
+  const store = useTodosStore()
   </script>
