81fd68b76ba0445ae8fc0f5761e324d7f712fe84
Oscar <oscarmtzz22@gmail.com>
2022-10-16 20:24:09 +0200

    feat marking all as completed

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="todo-header">
+        <input class="toggle" type="checkbox" v-model="checkAll" @change="checkAllChanged">
         <input class="user-input" type="text" v-model="userInput" @keyup.enter="action"
             placeholder="Type and press enter" autofocus>
     </div>
@@ -7,7 +8,9 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useTodosStore } from '../stores/todos'
 
+const store = useTodosStore()
 const emit = defineEmits(['click'])
 const checkAll = ref(false)
 let userInput = ref("")
@@ -16,7 +19,15 @@ function action() {
     emit('click', userInput.value)
     userInput.value = ''
 }
+async function checkAllChanged() {
+    await store.changeAllStatus(checkAll.value)
+}
 
+store.$onAction(({ name }) => {
+    if (name === 'clearCompleted') {
+        checkAll.value = false
+    }
+}, true)
 </script>
 
 <style scoped>
